abde43db943e3f3a1517233702a8eae39a862aab
zainabsirajo50 <75146677+zainabsirajo50@users.noreply.github.com>
2021-01-01 14:38:54 -0500

    Create SnitSoft.java

---
+++ SnitSoft.java
@@ -0,0 +1,19 @@
+public class SnitSoft {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double flashDrivePrice;
+		double shippingAndHandling;
+		double total;
+		
+		flashDrivePrice = 5.00;
+		shippingAndHandling = 0.00;
+		total = flashDrivePrice + shippingAndHandling + flashDrivePrice;
+		
+		System.out.print("We will bill $");
+		System.out.print(total);
+		System.out.println(" to your credit card.");
+		
+	}
+
+}
